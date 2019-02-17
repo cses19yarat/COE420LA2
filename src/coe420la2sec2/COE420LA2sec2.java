eb1667032b6cdf9f05ae01f2515b8f4a418a36f1
yarayayounette <g00065841@aus.edu>
2019-02-17 10:47:45 +0400

    Initial Version

---
+++ src/coe420la2sec2/COE420LA2sec2.java
@@ -0,0 +1,26 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package coe420la2sec2;
+
+/**
+ *
+ * @author g00065841
+ */
+public class COE420LA2sec2 {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void addFeatureA()
+    {
+        System.out.print("Hello world, I am member A");
+    }
+    public static void main(String[] args) {
+        // TODO code application logic here
+        addFeatureA();
+    }
+    
+}
